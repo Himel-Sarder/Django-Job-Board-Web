41c583cfe83958aecf3ad4eb3b398c8e642befbd
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-07 14:09:56 +0600

    Update views.py

--- views.py
+++ views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Hello World ! My name is Himel")
-
-
-
-
-
-
-
-
-# After creating myfirst.html file    
 from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
   template = loader.get_template('myfirst.html')
   return HttpResponse(template.render())
+
+def about(request):
+  template = loader.get_template('about.html')
+  return HttpResponse(template.render())
+
+def hello(request, first_name):
+  return HttpResponse(f'Hello {first_name}')
+
+def Sum(request, num1, num2):
+  return HttpResponse(f"The Sum of two number is {num1 + num2}")
