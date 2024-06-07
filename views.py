52abc6be210f658755a2b36110b5d9da8d536c15
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-07 12:07:53 +0600

    Update views.py

--- views.py
+++ views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World ! My name is Himel")
+
+
+
+
+
+
+
+
+# After creating myfirst.html file    
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
