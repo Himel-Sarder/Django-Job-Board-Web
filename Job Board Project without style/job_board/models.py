5872a8345decca522c691112b4e0272cc674276d
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-08 22:19:59 +0600

    Add files via upload

---
+++ Job Board Project without style/job_board/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+#job posting tables
+class JobPosting(models.Model):
+    # title, description, company, salary, location
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    location = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+
+
